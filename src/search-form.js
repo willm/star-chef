cb9464b84e9d686dc8dff71b49dc7e734e97f610
Will Munn <willmunn@hotmail.com>
2017-01-31 19:41:46 +0000

    Basic filtering by name

--- src/search-form.js
+++ src/search-form.js
@@ -1,8 +1,12 @@
 const React = require('react');
+require('whatwg-fetch');
 
 module.exports = React.createClass({
     getInitialState: function () {
-        return {q: ''}
+        return {
+            q: '',
+            results: []
+        }
     },
     handleChange: function (event) {
         this.setState({q: event.target.value});
@@ -11,13 +15,28 @@ module.exports = React.createClass({
         return <div>
             <div id="message">{this.state.message}</div>
             <form onSubmit={this.handleSubmit}>
-                <input type="text" name="" id="q" onChange={this.handleChange}/>
+                <input 
+                    type="text" name=""
+                    id="q" onChange={this.handleChange}/>
                 <button type="submit">Go!</button>
             </form>
+            <div>
+                {this.state.results.map(r => <p className="search-result">{r.name}</p>)}
+            </div>
         </div>
     },
     handleSubmit: function (event) {
-        this.setState({message: 'Sorry, nothing matched your filter term'});
+        fetch('/build/api/recipes.json')
+            .then(response => response.json())
+            .then(results => {
+                const filtered = results.filter(result => {
+                    return result.name.match(new RegExp(this.state.q));
+                });
+                this.setState({
+                    message: 'Sorry, nothing matched your filter term',
+                    results: filtered
+                });
+            });
         event.preventDefault();
     }
 });
