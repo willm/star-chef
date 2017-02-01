6866c6c5356aa2b5a071ca0434212ced77193e3d
Will Munn <willmunn@hotmail.com>
2017-02-01 00:56:34 +0000

    Deploy

--- src/js/get-recipes.js
+++ src/js/get-recipes.js
@@ -3,7 +3,7 @@ const mapSearch = require('./map-search');
 
 module.exports = function getRecipes(q, time, defaultState) {
     if (time > 0 || q && q.length > 3) {
-        return fetch('/build/api/recipes.json')
+        return fetch('build/api/recipes.json')
             .then(response => response.json())
             .then(results => {
                 return mapSearch(q, time, results);
